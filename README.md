68540b9763ae53a79d1bf399f03cce52c94440b1
Harsh Nayak <harsh.kumar8999@gmail.com>
2024-03-30 15:11:40 +0530

    database setup, log SLF4J setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# RentRead" 
+
+
+## Class Diagram
+https://lucid.app/lucidchart/46138806-4354-46a5-b75c-844a051a4537/edit?viewport_loc=-298%2C-78%2C2265%2C957%2C0_0&invitationId=inv_e6eb36ee-3cf5-4f23-9886-40b580ee6344
---
+++ src/test/java/com/harsh/RentRead/RentReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.RentRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
